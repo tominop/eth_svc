83b5152d6f2c39789fe8a29b5b6a6850b57f7c24
tominop <2tominop@gmail.com>
2018-01-12 12:15:20 +0500

    update initAPI

--- index.js
+++ index.js
@@ -1,31 +1,24 @@
 const express = require("express")
 const app = express()
 const axios = require('axios')
+const urlEth = 'https://rinkeby.infura.io/'
 var Web3 = require("web3")
-const ethUrl = 'https://rinkeby.infura.io/'
-var Eth3 = new Web3(new Web3.providers.HttpProvider(ethUrl))
+var Eth3 = ''
 
-app.get("/api/eth/:id", (req, res) => {
-    const addrsETH = req.params.id
+app.get("/eth/api/:token", (req, res) => {
+    const token = req.params.token
+    Eth3 = new Web3(new Web3.providers.HttpProvider(urlEth + token))
+    const provider = Eth3.currentProvider
+    res.header("Access-Control-Allow-Origin", "*")
+    res.json(provider)
+    console.log(provider)
+})
+
+app.get("/eth/balance/:addrs", (req, res) => {
+    const addrsETH = req.params.addrs
     const balance = Eth3.eth.getBalance(addrsETH) / 10 ** 18
     res.header("Access-Control-Allow-Origin", "*")
     res.json({ balance: balance })
-})
-
-app.get("/api/eth1/:id", (req, res) => {
-    const addrsETH = req.params.id
-    res.header("Access-Control-Allow-Origin", "*")
-    res.json({ balance: response.data.final_balance / 10 ** 8 })
-
-
-    axios.get(btcUrl + '/addrs/' + addrsETH + '/balance')
-        .then(response => {
-            //        console.log(response.data.url);
-            //        console.log(response.data.explanation);
-        })
-        .catch(error => {
-            console.log(error)
-        })
 })
 
 const port = process.env.PORT_ETH || 8200
